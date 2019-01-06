47ad334508b1311b1e0939a22c97a860dcaeea1f
Albert Qin <albert.qin@edu.uwaterloo.ca>
2019-01-05 21:05:17 -0500

    Discord BOT setup and basic tests

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ console.log('Bot is starting');
 
 var Twit = require('twit');
 var config = require('./config');
+var botconfig = require("./botconfig.json");
+var Discord = require("discord.js");
+
+
+//DISCORD BOT
+var bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArr = message.content.split(" ");
+  let command = messageArr[0];
+  let args = messageArr.slice(1);
+
+  if(command === `${prefix}raid`){
+    return message.channel.send("memes");
+  }
+})
+
+bot.login(botconfig.token);
 
 var T = new Twit(config)
 
@@ -12,7 +38,7 @@ var params = {
 
 T.get('search/tweets', params, gotData);
 
-
+//TWITTER API
 function gotData(err, data, response){
   var tweets = data.statuses;
   for (var i = 0; i < tweets.length; i++) {
