6baec7da9eddf5c32cb4dd676761eb73669063ca
Albert Qin <albert.qin@edu.uwaterloo.ca>
2019-01-05 22:47:10 -0500

    change contents

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'YnHyBc2nllMndlZiVrfaXAOZW',
+  consumer_secret:      'z8nBtcLMfOGXqfkh5rdGbIVPPq0FxYuGunJKDeZMw5zRjfiUAQ',
+  access_token:         '1081668004474105856-m8KXKjsPzEngf7vdLfUqEkE16cugEL',
+  access_token_secret:  '0LCZt7G95H1wmtrXohIcpgbjXJ7GQLIol3Y06G8tdaV23'
+}
